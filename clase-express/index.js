6a5e52204a657147cb785c6c146f78246f9c34f7
alejogomezcba <alejo.gomez@mercadolibre.com>
2021-12-17 16:28:53 -0300

    subiendo ejercicio

--- clase-express/index.js
+++ clase-express/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get("/home", (req,res) => {
+    res.redirect('/error')
+})
+
+//app.get("/home", (req,res) => {
+//    res.send('Estoy en el home')
+//})
+
+app.get("/error", (req,res) => {
+    res.send('Página en manteniento')
+})
+
 app.listen (8043, ()=> {
-console.log('Servidor levantado');
+console.log('Servidor levantado en puerto 8043');
 })
